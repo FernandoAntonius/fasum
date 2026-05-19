f9c2cde8507bbb373963fab7597dc1b3a89f0473
FernandoAntonius <fernandoanthonius_2428250014@mhs.mdp.ac.id>
2026-05-19 09:26:09 +0700

    push

--- fasum/lib/main.dart
+++ fasum/lib/main.dart
@@ -36,7 +36,6 @@ Future<void> showBasicNotification(String? title, String? body) async {
     priority: Priority.high,
     showWhen: true,
   );
-
   final platform = NotificationDetails(android: android);
   await flutterLocalNotificationsPlugin.show(0, title, body, platform);
 }
@@ -48,40 +47,19 @@ Future<void> showNotificationFromData(Map<String, dynamic> data) async {
   final time = data['sentAt'] ?? '';
   final photoUrl = data['senderPhotoUrl'] ?? '';
 
-  ByteArrayAndroidBitmap? largeIconBitmap;
-
-  if (photoUrl.isNotEmpty) {
-    final base64 = await _networkImageToBase64(photoUrl);
-
-    if (base64 != null) {
-      largeIconBitmap = ByteArrayAndroidBitmap.fromBase64String(base64);
-    }
-  }
-
-  final styleInfo = largeIconBitmap != null
-      ? BigPictureStyleInformation(
-          largeIconBitmap,
-          contentTitle: title,
-          summaryText: '$body\n\nDari: $sender - $time',
-          largeIcon: largeIconBitmap,
-          hideExpandedLargeIcon: true,
-        )
-      : BigTextStyleInformation(
-          '$body\n\nDari: $sender\nWaktu: $time',
-          contentTitle: title,
-        );
-
-  final simpleStyleInfo = BigTextStyleInformation(
-    '$body\n\nDari: $sender\nWaktu: $time',
-    contentTitle: title,
-  );
-
   final androidDetails = AndroidNotificationDetails(
     'detailed_channel',
     'Notifikasi Detail',
     channelDescription: 'Notifikasi dengan detail tambahan',
-    styleInformation: simpleStyleInfo,
-    largeIcon: largeIconBitmap,
+    styleInformation: BigTextStyleInformation(
+      '$body\n\nDari: $sender\nWaktu: $time',
+      contentTitle: title,
+    ),
+    largeIcon: photoUrl.isNotEmpty
+        ? ByteArrayAndroidBitmap.fromBase64String(
+            (await _networkImageToBase64(photoUrl)) ?? '',
+          )
+        : null,
     importance: Importance.max,
     priority: Priority.max,
   );
@@ -93,18 +71,15 @@ Future<void> showNotificationFromData(Map<String, dynamic> data) async {
 Future<String?> _networkImageToBase64(String url) async {
   try {
     final response = await http.get(Uri.parse(url));
-
     if (response.statusCode == 200) {
       return base64Encode(response.bodyBytes);
     }
   } catch (_) {}
-
   return null;
 }
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp();
-
   if (message.data.isNotEmpty) {
     await showNotificationFromData(message.data);
   } else {
@@ -124,12 +99,10 @@ void main() async {
 
   const AndroidInitializationSettings androidInit =
       AndroidInitializationSettings('@mipmap/ic_launcher');
-
   final InitializationSettings settings = InitializationSettings(
     android: androidInit,
     iOS: DarwinInitializationSettings(),
   );
-
   await flutterLocalNotificationsPlugin.initialize(settings);
 
   runApp(const MyApp());
@@ -154,16 +127,11 @@ class _MyAppState extends State<MyApp> {
 
   void setupFirebaseMessaging() async {
     String? token = await FirebaseMessaging.instance.getToken();
-
     print("FCM Token: $token");
 
     FirebaseMessaging messaging = FirebaseMessaging.instance;
-
     await messaging.subscribeToTopic(topic);
-
-    setState(() {
-      status = "Subscribed to topic: $topic";
-    });
+    setState(() => status = "Subscribed to topic: $topic");
 
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       if (message.data.isNotEmpty) {
