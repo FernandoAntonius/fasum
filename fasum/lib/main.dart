a382e07b94285c37caf8cbb20adb721f173a7cfc
FernandoAntonius <fernandoanthonius_2428250014@mhs.mdp.ac.id>
2026-05-20 10:25:33 +0700

    push

--- fasum/lib/main.dart
+++ fasum/lib/main.dart
@@ -1,11 +1,15 @@
 import 'dart:convert';
 import 'package:fasum/firebase_options.dart';
+import 'package:fasum/helper/locale_provider.dart';
+import 'package:fasum/l10n/app_localizations.dart';
 import 'package:fasum/screens/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
@@ -36,8 +40,14 @@ Future<void> showBasicNotification(String? title, String? body) async {
     priority: Priority.high,
     showWhen: true,
   );
+
   final platform = NotificationDetails(android: android);
-  await flutterLocalNotificationsPlugin.show(0, title, body, platform);
+  await flutterLocalNotificationsPlugin.show(
+    id: 0,
+    title: title,
+    body: body,
+    notificationDetails: platform,
+  );
 }
 
 Future<void> showNotificationFromData(Map<String, dynamic> data) async {
@@ -47,39 +57,68 @@ Future<void> showNotificationFromData(Map<String, dynamic> data) async {
   final time = data['sentAt'] ?? '';
   final photoUrl = data['senderPhotoUrl'] ?? '';
 
+  ByteArrayAndroidBitmap? largeIconBitmap;
+
+  if (photoUrl.isNotEmpty) {
+    final base64 = await _networkImageToBase64(photoUrl);
+
+    if (base64 != null) {
+      largeIconBitmap = ByteArrayAndroidBitmap.fromBase64String(base64);
+    }
+  }
+
+  final styleInfo = largeIconBitmap != null
+      ? BigPictureStyleInformation(
+          largeIconBitmap,
+          contentTitle: title,
+          summaryText: '$body\n\nDari: $sender - $time',
+          largeIcon: largeIconBitmap,
+          hideExpandedLargeIcon: true,
+        )
+      : BigTextStyleInformation(
+          '$body\n\nDari: $sender\nWaktu: $time',
+          contentTitle: title,
+        );
+
+  final simpleStyleInfo = BigTextStyleInformation(
+    '$body\n\nDari: $sender\nWaktu: $time',
+    contentTitle: title,
+  );
+
   final androidDetails = AndroidNotificationDetails(
     'detailed_channel',
     'Notifikasi Detail',
     channelDescription: 'Notifikasi dengan detail tambahan',
-    styleInformation: BigTextStyleInformation(
-      '$body\n\nDari: $sender\nWaktu: $time',
-      contentTitle: title,
-    ),
-    largeIcon: photoUrl.isNotEmpty
-        ? ByteArrayAndroidBitmap.fromBase64String(
-            (await _networkImageToBase64(photoUrl)) ?? '',
-          )
-        : null,
+    styleInformation: simpleStyleInfo,
+    largeIcon: largeIconBitmap,
     importance: Importance.max,
     priority: Priority.max,
   );
 
   final platform = NotificationDetails(android: androidDetails);
-  await flutterLocalNotificationsPlugin.show(1, title, body, platform);
+  await flutterLocalNotificationsPlugin.show(
+    id: 1,
+    title: title,
+    body: body,
+    notificationDetails: platform,
+  );
 }
 
 Future<String?> _networkImageToBase64(String url) async {
   try {
     final response = await http.get(Uri.parse(url));
+
     if (response.statusCode == 200) {
       return base64Encode(response.bodyBytes);
     }
   } catch (_) {}
+
   return null;
 }
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp();
+
   if (message.data.isNotEmpty) {
     await showNotificationFromData(message.data);
   } else {
@@ -99,13 +138,20 @@ void main() async {
 
   const AndroidInitializationSettings androidInit =
       AndroidInitializationSettings('@mipmap/ic_launcher');
+
   final InitializationSettings settings = InitializationSettings(
     android: androidInit,
     iOS: DarwinInitializationSettings(),
   );
-  await flutterLocalNotificationsPlugin.initialize(settings);
 
-  runApp(const MyApp());
+  await flutterLocalNotificationsPlugin.initialize(settings: settings);
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => LocaleProvider(),
+      child: const MyApp(),
+    ), // ChangeNotifierProvider
+  );
 }
 
 class MyApp extends StatefulWidget {
@@ -127,11 +173,16 @@ class _MyAppState extends State<MyApp> {
 
   void setupFirebaseMessaging() async {
     String? token = await FirebaseMessaging.instance.getToken();
+
     print("FCM Token: $token");
 
     FirebaseMessaging messaging = FirebaseMessaging.instance;
+
     await messaging.subscribeToTopic(topic);
-    setState(() => status = "Subscribed to topic: $topic");
+
+    setState(() {
+      status = "Subscribed to topic: $topic";
+    });
 
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       if (message.data.isNotEmpty) {
@@ -147,9 +198,22 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<LocaleProvider>(context);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Fasum',
+      locale: provider.locale,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('id', 'ID'), // Bahasa Indonesia
+        Locale('en', 'US'), // English
+      ],
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
